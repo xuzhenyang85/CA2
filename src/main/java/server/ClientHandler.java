edff02132567edff39c40e03cee3396e41eddf43
xuzhenyang85 <cph-xy171@cphbusiness.dk>
2017-10-03 10:56:05 +0200

    added .gitignore fil

---
+++ src/main/java/server/ClientHandler.java
@@ -0,0 +1,45 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClientHandler extends Thread {
+
+
+        Scanner reader;
+        PrintWriter writer;
+        Socket socket;
+        EchoServer master;
+
+        public ClientHandler(Socket socket, EchoServer master) throws IOException {
+            this.socket = socket;
+            this.master = master;
+            reader = new Scanner(socket.getInputStream());
+            writer = new PrintWriter(socket.getOutputStream(), true);
+        }
+
+        @Override
+        public void run() {
+            this.master.addClientHandler(this);
+            String message = reader.nextLine(); //IMPORTANT blocking call
+            System.out.println("Received " + message);
+            while (!message.equals("stop")) {
+                master.echoMessageToAll(message);
+                message = reader.nextLine();
+            }
+            try {
+                this.master.removeClientHandler(this);
+                socket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        void sendMessage(String msg) {
+            writer.println(msg);
+        }
+
+}
