d7d3b6772b2a0d3cb56db358d551832ac2888865
menjaw <menjaholm@gmail.com>
2017-10-04 11:03:13 +0200

    Login and logout method done in echoClient

--- src/main/java/client/EchoClient.java
+++ src/main/java/client/EchoClient.java
@@ -19,11 +19,12 @@ import java.util.logging.Logger;
 public class EchoClient extends Thread implements IEchoClient {
 
     //Variables
+    private String user;
     public Socket clientSocket;
     private Scanner input;
     private PrintWriter output;
     private IDataReady observer;
-    private boolean running = true;
+    private volatile boolean running = true;
 
     //Add observer to check if the observed object is changing (in this case the message)
     @Override
@@ -39,19 +40,28 @@ public class EchoClient extends Thread implements IEchoClient {
             input = new Scanner(clientSocket.getInputStream());//Makes sure to take the clients input
             output = new PrintWriter(clientSocket.getOutputStream(), true);//Returns an output for this socket
             this.start();
+
         } catch (IOException ex) {
             Logger.getLogger(EchoClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     @Override
-    public void sendMessage(String message) {
-        output.println("MSG: " + message);
+    public void sendMessage(String message, String user) {
+        output.println(user + " " + message);
+    }
+
+    @Override
+    public void login(String user) {
+        output.println("LOGIN:" + user);
+        if (user.isEmpty()) {
+           
+        }
     }
 
     @Override
     public void closeConnection() {
-        sendMessage("Stop");
+        output.println("LOGOUT:");
         running = false;
     }
 
@@ -59,8 +69,9 @@ public class EchoClient extends Thread implements IEchoClient {
     @Override
     public void run() {
         while (running) {
-            String msg = input.nextLine();
+            String msg = input.nextLine(); //Læser på socket fra serveren
             observer.messageReady(msg);
+
         }
         try {
             clientSocket.close();
